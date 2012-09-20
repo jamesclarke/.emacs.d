2897aa0a322cb5f7356be09d0c6e6f44b3d791d6
James Clarke <jclarke@touchtype-online.com>
2012-09-20 17:42:42 +0100

    Added custom theme selection.

--- custom.el
+++ custom.el
@@ -3,11 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
- '(custom-enabled-themes (quote (wheatgrass)))
- '(custom-safe-themes (quote ("6cfe5b2f818c7b52723f3e121d1157cf9d95ed8923dbc1b47f392da80ef7495d" "fee4bfb5cfc4912d443840127d10b3ef64918bf510c181d7cafff4fb1510a1a2" "2b7d55fde75d5636ed0d5bf33d1b06ac75162f06816abe00522692e7daddc9ec" default)))
- '(zen-encumbered-urls (quote ("#brief timewastes" "www.penny-arcade.com" "www.dilbert.com" "www.xkcd.com" "www.userfriendly.org" "#news waste" "slashdot.org" "dn.se" "#social timewastes" "https://www.facebook.com")))
- '(zen-fullscreen-mode t))
+ '(custom-enabled-themes (quote (wheatgrass))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
